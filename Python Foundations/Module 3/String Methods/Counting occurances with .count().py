318e7a18e7952439a5b8ae4117dce7315da61420
Branice Nafula <nafula.branice@students.jkuat.ac.ke>
2026-06-16 14:05:11 +0300

    submit codes

---
+++ Python Foundations/Module 3/String Methods/Counting occurances with .count().py
@@ -0,0 +1,10 @@
+sentence = "the quick brown fox jumps over the lazy dog the"
+print(sentence.count("the"))
+print(sentence.count("The"))
+
+sentence = "aaaaa"
+print(sentence.count("aa"))
+print(sentence.count("a"))
+
+message = "I love:) you:)"
+print(message.count(":)"))
---
+++ Python Foundations/Module 3/String Methods/Finding text with .file().py
@@ -0,0 +1,8 @@
+sentence = "I love Python programming"
+pos = sentence.find("Python")
+print(pos)
+print(sentence.find("Java"))
+
+filename = "programming.pdf"
+pos = filename.find(".pdf")
+print(pos)
