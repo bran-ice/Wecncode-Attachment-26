d17642cc714cd8018879f460c69befeaa6da77d7
Branice Nafula <nafula.branice@students.jkuat.ac.ke>
2026-06-21 13:31:44 +0300

    save topic 4

---
+++ Python Foundations/module 4 Lists and loops/Topic 4 for loops/Iterating through a list.py
@@ -0,0 +1,11 @@
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+
+colors = ["red", "green", "blue"]
+for color in colors:
+    print("Color:", color)
+
+donated_books = ["The Hidden Life", "Ocean Light", "Map of Stars"]
+for book in donated_books:
+    print("Donated book:", book)
