6fc13ae0481229365ca6d96a51cfaeeb83101ec5
bran-ice <nafula.branice@students.jkuat.ac.ke>
2026-07-13 23:01:16 +0300

    Add user transactions for SAF-992834-KE
    Added a new user entry with transaction details.

--- WEEK 3/Assignments/Tech company JSON files Safaricom/JSON.py
+++ WEEK 3/Assignments/Tech company JSON files Safaricom/JSON.py
@@ -11,4 +11,11 @@ import json
   "is_frozen": False
    }
    {
+  "user_id": "SAF-992834-KE",
+  "transactions": [
+    {"tx_id": "TXN-88712", "amount": 500, "type": "pay_bill", "merchant": "KPLC"},
+    {"tx_id": "TXN-88715", "amount": 1200, "type": "send_money", "recipient": "+254799887766"}
+    ]
+    }
+
     
