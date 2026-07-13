fe240371dfb8c8b38bc30d853d41daf0139b6351
bran-ice <nafula.branice@students.jkuat.ac.ke>
2026-07-13 22:59:22 +0300

    Update and rename account_details.py to JSON.py
    created one consolidated file

--- WEEK 3/Assignments/Tech company JSON files Safaricom/JSON.py
+++ WEEK 3/Assignments/Tech company JSON files Safaricom/JSON.py
@@ -1,7 +1,7 @@
 
 import json
-
 {
+   {
   "user_id": "SAF-992834-KE",
   "account_type": "m-pesa_personal",
   "balance": 15450.50,
@@ -9,4 +9,6 @@ import json
   "last_transaction_timestamp": "2026-07-01T10:15:22Z",
   "daily_limit_usage": 45000,
   "is_frozen": False
-}
+   }
+   {
+    
