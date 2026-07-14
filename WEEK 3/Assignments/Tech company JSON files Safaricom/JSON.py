419b28e8362ec1583634f0151e0601df045a71fd
bran-ice <nafula.branice@students.jkuat.ac.ke>
2026-07-14 12:48:31 +0300

    Update JSON structure for user data
    Replaced old user data structure with a new JSON format containing transaction records and user details.

--- WEEK 3/Assignments/Tech company JSON files Safaricom/JSON.py
+++ WEEK 3/Assignments/Tech company JSON files Safaricom/JSON.py
@@ -1,40 +1,98 @@
-
-import json
-{
-   {
-  "user_id": "SAF-992834-KE",
-  "account_type": "m-pesa_personal",
-  "balance": 15450.50,
-  "currency": "KES",
-  "last_transaction_timestamp": "2026-07-01T10:15:22Z",
-  "daily_limit_usage": 45000,
-  "is_frozen": False
-   }
-   {
-  "user_id": "SAF-992834-KE",
-  "transactions": [
-    {"tx_id": "TXN-88712", "amount": 500, "type": "pay_bill", "merchant": "KPLC"},
-    {"tx_id": "TXN-88715", "amount": 1200, "type": "send_money", "recipient": "+254799887766"}
-      ]
-    }
-    {
-  "user_id": "SAF-992834-KE",
-  "auth_method": "biometric_and_password",
-  "mfa_enabled": True,
-  "password_last_changed": "2026-01-10",
-  "failed_login_attempts": 0,
-  "last_ip_address": "192.168.1.105"
-    }
-     {
-  "user_id": "SAF-992834-KE",
-  "active_bundle": {
-    "plan_name": "Platinum_10GB",
-    "expiry_date": "2026-07-15T23:59:59Z",
-    "remaining_data_mb": 4500
-         },
-  "roaming_enabled": False,
-  "last_network_access": "2026-07-01T13:45:00Z"
-      }
-
-
-    
+[
+  {
+    "user_id": "SAF-571131-KE",
+    "tx_id": "TXN-86116",
+    "amount": 2262.12,
+    "type": "buy_goods",
+    "merchant": "SUPERMARKET",
+    "timestamp": "2026-06-19T09:44:45"
+  },
+  {
+    "user_id": "SAF-253396-KE",
+    "tx_id": "TXN-24105",
+    "amount": 3102.59,
+    "type": "send_money",
+    "merchant": "BETTING",
+    "timestamp": "2026-06-23T09:44:45"
+  },
+  {
+    "user_id": "SAF-957058-KE",
+    "tx_id": "TXN-81923",
+    "amount": 1273.62,
+    "type": "buy_goods",
+    "merchant": "AIRTIME",
+    "timestamp": "2026-06-14T09:44:45"
+  },
+  {
+    "user_id": "SAF-913748-KE",
+    "tx_id": "TXN-28841",
+    "amount": 397.91,
+    "type": "send_money",
+    "merchant": "SUPERMARKET",
+    "timestamp": "2026-06-26T09:44:45"
+  },
+  {
+    "user_id": "SAF-790958-KE",
+    "tx_id": "TXN-84704",
+    "amount": 2571.89,
+    "type": "buy_goods",
+    "merchant": "KPLC",
+    "timestamp": "2026-06-17T09:44:45"
+  },
+  {
+    "user_id": "SAF-783099-KE",
+    "tx_id": "TXN-80601",
+    "amount": 387.86,
+    "type": "pay_bill",
+    "merchant": null,
+    "timestamp": "2026-07-07T09:44:45"
+  },
+  {
+    "user_id": "SAF-319943-KE",
+    "tx_id": "TXN-66045",
+    "amount": 4278.01,
+    "type": "buy_goods",
+    "merchant": null,
+    "timestamp": "2026-06-26T09:44:45"
+  },
+  {
+    "user_id": "SAF-153243-KE",
+    "tx_id": "TXN-28819",
+    "amount": 2268.85,
+    "type": "pay_bill",
+    "merchant": "PAYBILL",
+    "timestamp": "2026-06-26T09:44:45"
+  },
+  {
+    "user_id": "SAF-688887-KE",
+    "tx_id": "TXN-76157",
+    "amount": 355.75,
+    "type": "pay_bill",
+    "merchant": "PAYBILL",
+    "timestamp": "2026-07-05T09:44:45"
+  },
+  {
+    "user_id": "SAF-736556-KE",
+    "tx_id": "TXN-45555",
+    "amount": 1224.89,
+    "type": "send_money",
+    "merchant": null,
+    "timestamp": "2026-07-14T09:44:45"
+  },
+  {
+    "user_id": "SAF-739810-KE",
+    "tx_id": "TXN-72746",
+    "amount": 4980.49,
+    "type": "pay_bill",
+    "merchant": null,
+    "timestamp": "2026-06-28T09:44:45"
+  },
+  {
+    "user_id": "SAF-849181-KE",
+    "tx_id": "TXN-31045",
+    "amount": 4202.03,
+    "type": "buy_goods",
+    "merchant": null,
+    "timestamp": "2026-07-14T09:44:45"
+  }
+]
