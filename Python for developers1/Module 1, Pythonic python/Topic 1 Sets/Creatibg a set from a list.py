45c3beb42cbab27e5be223fbe082488b14a2e8cd
Branice Nafula <nafula.branice@students.jkuat.ac.ke>
2026-07-16 19:35:49 +0300

    add this file

---
+++ Python for developers1/Module 1, Pythonic python/Topic 1 Sets/Creatibg a set from a list.py
@@ -0,0 +1,7 @@
+colors_list = ["red", "blue", "red", "green"]
+unique_colors = set(colors_list)
+print(unique_colors)
+
+Programming_languages = ["C", "C++", "Python", "C", "Java"]
+unique_languages = set(Programming_languages)
+print(unique_languages)
